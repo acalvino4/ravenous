841cce0e5520fe0db1b2819ed717f0d48b88bd7b
Augustine Calvino <43254861+acalvino4@users.noreply.github.com>
2019-05-09 11:17:41 -0600

    Fix sortBy typo

--- src/components/SearchBar/SearchBar.js
+++ src/components/SearchBar/SearchBar.js
@@ -3,7 +3,7 @@ import './SearchBar.css'
 
 const sortByOptions = {
     "Best Match": "best_match",
-    "Hightest Rated": "rating",
+    "Highest Rated": "rating",
     "Most Reviewed": "review_count"
 }
 
